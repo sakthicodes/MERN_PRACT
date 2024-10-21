366534472dcff35cd2a98d568fa76abe759b3fea
sakthicodes <sakthikumaran14122002@gmail.com>
2024-10-21 19:29:17 +0530

    app structure

--- app.js
+++ app.js
@@ -1,31 +1,5 @@
-const express = require('express');
-const http = require('http')
+import express from "express";
 const app = express();
-const path = require('path')
-const server = http.createServer(app);
-server.listen(8004);
-
-app.use((req,res,next)=>{
-    res.status(200);
-   next();
-})
-app.get('^/$|/index.html',(req,res)=>{
-    res.status(200).sendFile(path.join(__dirname,'views','index.html'))
-})
-
-
-const one = (req,res,next) => {
-     console.log("first")
-     next()
-}
-const two = (req,res,next) => {
-    console.log("seond")
-    res.send("api")
-
-}
-app.get('/api',[one,two])
-
-
-app.get('/*',(req,res)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
-})
+import path from "path";
+const port = 8002;
+app.listen(port);
